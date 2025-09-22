ad48fb01b0676c932fd97c534a493d7a5e050429
Mikel <mikeljoshnicer@gmail.com>
2025-09-22 21:58:02 +0800

    feature(number_guessing): complete app setup

--- number_guessing/numberguessing/views.py
+++ number_guessing/numberguessing/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+def play(request):
+    number = random.randint(1, 100)
+    message = None
+
+    if request.method == "POST":
+        guess = int(request.POST.get("guess"))
+        if guess == number:
+            message = "🎉 Correct! You guessed it!"
+        elif guess < number:
+            message = "Too low! Try again."
+        else:
+            message = "Too high! Try again."
+
+    return render(request, "game/play.html", {"message": message})
